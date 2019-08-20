63be54e58aa3e35931050a87442cb99c06cc7bc3
Rachel Wyatt <rw126@evansville.edu>
2019-08-20 08:34:29 -0400

    stub out user action and refactor organization

--- src/index.js
+++ src/index.js
@@ -6,10 +6,15 @@ import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import usersReducer from './reducers/users.js'
+
+const reducer = combineReducers({
+    user: usersReducer
+})
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const store = createStore(composeEnhancer(applyMiddleware(thunk)) )
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)) )
 
 ReactDOM.render(<Provider store={ store }><App /></Provider>, document.getElementById('root'));
 
