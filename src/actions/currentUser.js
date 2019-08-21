59615d7d99d94d223790ace07a08cc16f4f5522d
Rachel Wyatt <rw126@evansville.edu>
2019-08-21 16:47:55 -0400

    mock up login action

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -4,3 +4,16 @@ export const setCurrentUser = user => {
         user
     }
 }
+
+//async action creators
+export const login = credentials => {
+    return dispatch => {
+        return fetch("http://localhost:3000/v1/api/login"), {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({email: "dave@cat.com", password: "password"})
+        }
+    }
+}
