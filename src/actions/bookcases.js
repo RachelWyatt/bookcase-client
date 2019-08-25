4b79945d9a9e735877ba26c6f588c12ef5a83a7e
Rachel Wyatt <rw126@evansville.edu>
2019-08-25 17:51:21 -0400

    create getMyBookcases action to fetch bookcase data

---
+++ src/actions/bookcases.js
@@ -0,0 +1,26 @@
+export const myBookcases = bookcases => {
+    return {
+        type: "SET_MY_BOOKCASES",
+        bookcases
+    }
+}
+
+
+export const getMyBookcases = () => {
+    return dispatch => {
+        return fetch("http://localhost:3001/api/v1/bookcases", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => response.json())
+        .then(response => {
+            if (response.error) {
+                alert(response.error)
+            } else {
+                dispatch(myBookcases([]))
+            }
+        })
+    }
+}
