30f07ef2d15c59425d223e937d5fa3c4799d47ae
Rachel Wyatt <rw126@evansville.edu>
2019-08-25 19:55:20 -0400

    update bookcase reducer to update store state

--- src/reducers/bookcases.js
+++ src/reducers/bookcases.js
@@ -1,5 +1,7 @@
 export default (state = [], action) => {
     switch(action.type) {
+        case "SET_MY_BOOKCASES":
+            return action.bookcases
         default: 
             return state
     }
