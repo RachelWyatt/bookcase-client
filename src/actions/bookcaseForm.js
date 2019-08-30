470b5f6ae39e578beca1bd52fa6173941651fbd3
Rachel Wyatt <rw126@evansville.edu>
2019-08-30 07:19:33 -0400

    add actions to console.log new trip when made

--- src/actions/bookcaseForm.js
+++ src/actions/bookcaseForm.js
@@ -1,12 +1,38 @@
-export const updateBookcaseForm = (formData) => {
+export const updateBookcaseForm = (name, value) => {
     return {
         type: "UPDATE_BOOKCASE_FORM",
-        formData
+        formData: { name, value }
     }
 }
 
 export const resetBookcaseForm = () => {
     return {
         type: "RESET_SIGNUP_FORM"
+    }
+}
+
+export const addBookcase = bookcase => {
+    return {
+        type: "ADD_BOOKCASE",
+        bookcase
+    }
+}
+
+export const buildBookcase = bookcaseData => {
+    return dispatch => {
+        const sendableBookcaseData = {
+            bookcase: {
+                name: bookcaseData.name  
+            }
+        }
+        return fetch("http://localhost:3001/api/v1/bookcases", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(sendableBookcaseData)
+        })
+        .then(response => response.json())
+        .then(console.log)
     }
 }
