913d492b4477a79323056a5ed084b1e8c031165b
Rachel Wyatt <rw126@evansville.edu>
2019-08-29 20:07:05 -0400

    create form for new bookcase

--- src/components/NewBookcase.js
+++ src/components/NewBookcase.js
@@ -1,35 +1,34 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { updateBookcaseForm } from '../actions/bookcaseForm.js'
+import { buildBookcase } from '../actions/bookcaseForm.js'
 
-const BookcaseForm = ({ bookcaseFormData, updateBookcaseForm }) => {
+const BookcaseForm = ({ formData, history, updateBookcaseForm, buildBookcase }) => {
+    const { name } = formData
 
-const handleInputChange = event => {
-    const { name, value } = event.target
-    const updatedBookcaseForm = {
-        ...bookcaseFormData,
-        [name]: value
+    const handleInputChange = event => {
+        const { name, value } = event.target
+        updateBookcaseForm(name, value)
     }
-    updateBookcaseForm(updatedBookcaseForm)
-}
 
-const handleSubmit = event => {
-    event.preventDefault()
-    //buildBookcase(bookcaseFormData)
-}
-
-return (
-    < form onSubmit={handleSubmit}>
-        <input placeholder="Bookcase Name" value={bookcaseFormData} name="bookcase-name" type="text" onChange={handleInputChange}/>
-        <input type="submit" value="Submit"/>
-    </form>
-)
-}
-
-const mapStateToProps = state => {
-    return {
-        bookcaseFormData: state.BookcaseForm
+    const handleSubmit = event => {
+        event.preventDefault()
+        buildBookcase(formData)
     }
+
+    return (
+        < form onSubmit={handleSubmit}>
+            <input placeholder="Name" value={name} name="name" type="text" onChange={handleInputChange}/>
+            <input type="submit" value="Submit"/>
+        </form>
+    )
+    }
+
+    const mapStateToProps = state => {
+        return {
+            formData: state.bookcaseForm
+        }
 }
 
-export default connect(mapStateToProps, { updateBookcaseForm })(BookcaseForm)
+export default connect(mapStateToProps, { updateBookcaseForm, buildBookcase })(BookcaseForm)
+// { updateBookcaseForm, buildBookcase } is shorthand syntax for MapDispatchtoProps
