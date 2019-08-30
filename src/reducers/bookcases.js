0f0ca09e0c023e9533c33c8203594a5889bb4c1e
Rachel Wyatt <rw126@evansville.edu>
2019-08-29 20:05:51 -0400

    Add ADD_BOOKCASE reducer

--- src/reducers/bookcases.js
+++ src/reducers/bookcases.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
     switch(action.type) {
         case "SET_MY_BOOKCASES":
             return action.bookcases
+        case "ADD_BOOKCASE":
+            return state.bookcases.concat(action.bookcases)            
         default: 
             return state
     }
