aa07186922307cfd84c4f5ab41fc1caa1156f93c
Rachel Wyatt <rw126@evansville.edu>
2019-09-01 09:49:30 -0400

    update App.js notes

--- src/App.js
+++ src/App.js
@@ -19,12 +19,6 @@ class App extends React.Component {
           <h1> Bookcase Builder </h1>
           < BookcasesContainer />
           <Route exact path= '/bookcases/new' component={ NewBookcase }/>
-            <h3> New Bookcase Option </h3>
-            <h2> To be built: Books Container </h2>
-              <ul>
-                <li>Show books from selected bookcase</li>
-              </ul>
-            <h3>New Book Option</h3>
       </div>
     </Router>
   );
