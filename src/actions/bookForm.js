f67fddbb5d1f0571923318a606e2c895c0be63b2
Rachel Wyatt <rw126@evansville.edu>
2019-09-02 11:15:12 -0400

    add create book function, console log book state

--- src/actions/bookForm.js
+++ src/actions/bookForm.js
@@ -11,9 +11,31 @@ export const resetBookForm = () => {
     }
 }
 
-export const addBook = book => {
-    return {
-        type: "ADD_BOOK",
-        book
+
+export const createBook = (formData) => {
+    return dispatch => {
+        const sendableBookData = {
+            book: {
+                title: formData.name,
+                author: formData.author,
+                page_count: formData.pageCount,
+                spine_color: formData.spineColor, 
+                bookcase_id: formData.bookcaseId  
+            }
+        }
+        return fetch(`http://localhost:3001/api/v1/bookcases/${formData.bookcaseId}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(sendableBookData)
+        })
+        .then(response => response.json())
+        .then(response => {
+            if (response.error) {
+                alert(response.error)
+            } else { 
+                dispatch(console.log(response.data))}
+        })
     }
 }
