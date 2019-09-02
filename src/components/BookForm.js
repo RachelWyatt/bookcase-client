24036990a24608c346a8b99b4366726f9380fd35
Rachel Wyatt <rw126@evansville.edu>
2019-09-02 09:45:28 -0400

    add onChange handlers to inputs

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -4,12 +4,16 @@ import { updateBookForm } from '../actions/bookForm.js'
 
 const BookForm = ( {formData, updateBookForm}) => {
     const { title, author, pageCount, spineColor } = formData
+    const handleChange = event => {
+        const {name, value} = event.target
+        updateBookForm(name, value)
+    }
     return (
         < form>
-            <input placeholder="Title" name="name" type="text" value={title}/> <br/>
-            <input placeholder="Author" name="author" type="text" /><br/>
-            <input placeholder="Page count" name="page-count" type="integer" /><br/>
-            <input placeholder="Spine color" name="spine-color" type="text" /><br/>
+            <input placeholder="Title" name="title" type="text" onChange={handleChange}/> <br/>
+            <input placeholder="Author" name="author" type="text" onChange={handleChange}/><br/>
+            <input placeholder="Page count" name="pageCount" type="integer" onChange={handleChange}/><br/>
+            <input placeholder="Spine color" name="spineColor" type="text" onChange={handleChange}/><br/>
             <input type="submit" value="Submit"/>
         </form>
     )
