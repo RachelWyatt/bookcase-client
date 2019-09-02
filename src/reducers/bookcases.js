47789edff070474f4514afe758acdbdf3edd18e7
Rachel Wyatt <rw126@evansville.edu>
2019-09-02 16:40:45 -0400

    edit ADD_BOOK to only edit bookcase if the payload id matches bookcase id

--- src/reducers/bookcases.js
+++ src/reducers/bookcases.js
@@ -1,9 +1,17 @@
-export default (state = [], action) => {
+export default (state = {bookcases: []}, action) => {
     switch(action.type) {
         case "SET_MY_BOOKCASES":
             return action.bookcases
         case "ADD_BOOKCASE":
-            return state.concat(action.bookcase)            
+            return state.concat(action.bookcase)
+        case "ADD_BOOK": 
+            return {...state, bookcases: state.map(bookcase => {
+                if (bookcase.id === action.payload.id) {
+                    return action.payload
+                } else {
+                    return bookcase
+                }
+            })}            
         default: 
             return state
     }
