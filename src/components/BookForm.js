418b57b7ee1bc2b10bb55df39b5da3055cea9519
Rachel Wyatt <rw126@evansville.edu>
2019-09-02 09:40:39 -0400

    connect BookForm to redux store and grab state

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { updateBookForm } from '../actions/bookForm.js'
 
-const BookForm = () => {
-
+const BookForm = ( {formData, updateBookForm}) => {
+    const { title, author, pageCount, spineColor } = formData
     return (
         < form>
-            <input placeholder="Title" name="name" type="text" /> <br/>
+            <input placeholder="Title" name="name" type="text" value={title}/> <br/>
             <input placeholder="Author" name="author" type="text" /><br/>
             <input placeholder="Page count" name="page-count" type="integer" /><br/>
             <input placeholder="Spine color" name="spine-color" type="text" /><br/>
@@ -13,4 +15,11 @@ const BookForm = () => {
     )
     }
 
-export default BookForm
+    const mapStateToProps = state => {
+        return {
+          formData: state.bookForm
+        }
+      }
+      
+
+export default connect(mapStateToProps, { updateBookForm })(BookForm)
