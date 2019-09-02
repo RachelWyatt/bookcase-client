5639085fed72c5e78924eb81f670d7548c62cf48
Rachel Wyatt <rw126@evansville.edu>
2019-09-02 13:20:42 -0400

    remove ADD_BOOK case from bookForm reducer to bookcases.js reducer

--- src/reducers/bookForm.js
+++ src/reducers/bookForm.js
@@ -14,8 +14,7 @@ export default (state=initialState, action) => {
             }
         case 'RESET_BOOK_FORM':
             return initialState
-        case 'ADD_BOOK':
-            return state.myBookcases.books.concat(action.book)
+
         default:
             return state
     }
