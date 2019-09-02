5fdbfdb15a5baca6f23ecf496f17deaa5ba88a38
Rachel Wyatt <rw126@evansville.edu>
2019-09-02 11:37:20 -0400

    refactor BookForm into a class that holds its own state

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,36 +1,34 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { updateBookForm } from '../actions/bookForm.js'
 import { addBook } from '../actions/bookForm.js'
 
-const BookForm = ( {formData, updateBookForm, bookcaseId, addBook}) => {
-    const { title, author, pageCount, spineColor } = formData
-    const handleChange = event => {
-        const {name, value} = event.target
-        updateBookForm(name, value)
+class BookForm extends React.Component {
+    state = {
+        title: "",
+        author: "",
+        pageCount: "",
+        spineColor: "",
     }
-    const handleSubmit = event => {
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+          })
+    }
+    handleSubmit = event => {
         event.preventDefault()
-        addBook(formData)
     }
+    render() {
     return (
-        < form onSubmit={handleSubmit}>
-            <input placeholder="Title" name="title" type="text" onChange={handleChange}/> <br/>
-            <input placeholder="Author" name="author" type="text" onChange={handleChange}/><br/>
-            <input placeholder="Page count" name="pageCount" type="integer" onChange={handleChange}/><br/>
-            <input placeholder="Spine color" name="spineColor" type="text" onChange={handleChange}/><br/>
+        < form onSubmit={this.handleSubmit}>
+            <input placeholder="Title" name="title" type="text" onChange={this.handleChange}/> <br/>
+            <input placeholder="Author" name="author" type="text" onChange={this.handleChange}/><br/>
+            <input placeholder="Page count" name="pageCount" type="integer" onChange={this.handleChange}/><br/>
+            <input placeholder="Spine color" name="spineColor" type="text" onChange={this.handleChange}/><br/>
             <input type="submit" value="Submit"/>
         </form>
     )
     }
-
-    const mapStateToProps = state => {
-        const bookcaseId = state.myBookcases.id
-        return {
-          formData: state.bookForm,
-          bookcaseId: bookcaseId
-        }
-      }
+}
       
 
-export default connect(mapStateToProps, { updateBookForm, addBook })(BookForm)
+export default connect(null, { addBook })(BookForm)
