84863b75653f7c99eae2a6f121f32c340f6a099b
Rachel Wyatt <rw126@evansville.edu>
2019-09-03 19:48:34 -0400

    add conditional to only render books if props are present

--- src/components/BooksContainer.js
+++ src/components/BooksContainer.js
@@ -3,6 +3,7 @@ import BookForm from './BookForm.js'
 import { Link } from 'react-router-dom'
 
 const BooksContainer = (props) => {
+if (props.bookcases.length > 1) {
     const bookcase = props.bookcases.filter( bookcase => bookcase.id === props.match.params.id)[0]
     const books = bookcase.attributes.books.map(b => (
         <div class="book " key={b.id} style={
@@ -12,16 +13,17 @@ const BooksContainer = (props) => {
             <h3>{b.author}</h3>
 
         </div>))
+            return (
+                <div>
+                    <h2>{bookcase.attributes.name}</h2>
+                    <div class = "bookshelf">
+                        {books} <br/>
+                    </div>
+                    <BookForm bookcase={bookcase} bookcaseId = {bookcase.id} history={props.history} />
+                </div>
+            )
+} else return <p>Loading...</p>
 
-    return (
-        <div>
-            <h2>{bookcase.attributes.name}</h2>
-            <div class = "bookshelf">
-                {books} <br/>
-            </div>
-            <BookForm bookcase={bookcase} bookcaseId = {bookcase.id} />
-        </div>
-    )
 }
 
 
