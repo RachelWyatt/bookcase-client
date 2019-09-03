5fc64bac24a24623a71fc2d514c620215f7fd63d
Rachel Wyatt <rw126@evansville.edu>
2019-09-03 18:51:31 -0400

    add conditional to BookcasesContainer to only render if props have been fetched

--- src/components/BookcasesContainer.js
+++ src/components/BookcasesContainer.js
@@ -3,10 +3,12 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const Bookcases = props => {
+  if (props.bookcases.length > 1) {
       const bookcaseCards = props.bookcases.map(b => (<li key={b.id}><Link to={`/bookcases/${b.id}`}>{b.attributes.name}</Link></li>))
       return (
         bookcaseCards
       )
+  } else return <p>Loading...</p>
   }
 
   const mapStateToProps = (state) => {
