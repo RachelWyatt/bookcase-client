ea5ff287e31b9b5890b99c78070f6d848f92824a
Rachel Wyatt <rw126@evansville.edu>
2019-09-04 09:19:15 -0400

    add styling to New Book Form

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -20,16 +20,37 @@ class BookForm extends React.Component {
     }
     render() {
     return (
-        <div>
+        <div class="ml-5">
         <h2>Add a Book</h2>
         < form onSubmit={this.handleSubmit}>
-            <input placeholder="Title" name="title" value={this.props.title} type="text" onChange={this.handleChange}/> <br/>
-            <input placeholder="Author" name="author" value={this.props.author} type="text" onChange={this.handleChange}/><br/>
-            <input placeholder="Page count" name="pageCount" value={this.props.pageCount} type="integer" onChange={this.handleChange}/><br/>
-            <input placeholder="Spine color" name="spineColor" value={this.props.spineColor} type="text" onChange={this.handleChange}/><br/>
-            <input type="submit" value="Submit"/>
+            <div class="form-group row">
+                <label for="bookFormTitleInput" class="col-sm-2 col-form-label">Title</label>
+                <div class="col-sm-6">
+                    <input placeholder="Title" class="form-control" name="title" value={this.props.title} type="text" onChange={this.handleChange}/> 
+                </div>
+            </div>
+            <div class="form-group row" >
+                <label for="bookFormAuthorInput" class="col-sm-2 col-form-label">Author</label>
+                <div class="col-sm-6">
+                    <input placeholder="Author"  class="form-control" name="author" value={this.props.author} type="text" onChange={this.handleChange}/>
+                </div>
+            </div>
+            <div class="form-group row" >
+                <label for="bookFormPageCountInput" class="col-sm-2 col-form-label">Page Count</label>
+                <div class="col-sm-6">
+                    <input placeholder="Page count"  class="form-control"  name="pageCount" value={this.props.pageCount} type="integer" onChange={this.handleChange}/>
+                </div>
+            </div>
+            <div class="form-group row" >
+                <label for="bookFormSpineColorInput" class="col-sm-2 col-form-label">Spine Color</label>
+                <div class="col-sm-6">
+                    <input placeholder="Spine color"  class="form-control"  name="spineColor" value={this.props.spineColor} type="text" onChange={this.handleChange}/>
+                </div>
+            </div>
+            <input type="submit" value="Submit" class="btn btn-outline-dark"/>
         </form>
         </div>
+        
     )
     }
 }
