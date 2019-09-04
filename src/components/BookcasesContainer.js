af4c52d26b319d67213f2d67ec511bd6c314432e
Rachel Wyatt <rw126@evansville.edu>
2019-09-04 10:23:52 -0400

    add list styling to bookcase links

--- src/components/BookcasesContainer.js
+++ src/components/BookcasesContainer.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-
+import { List } from 'reactstrap';
 const Bookcases = props => {
   if (props.bookcases.length > 1) {
-      const bookcaseCards = props.bookcases.map(b => (<li key={b.id}><Link to={`/bookcases/${b.id}`}>{b.attributes.name}</Link></li>))
+      const bookcaseCards = props.bookcases.map(b => (<li class="list-group-item"><Link to={`/bookcases/${b.id}`} >{b.attributes.name}</Link></li>))
       return (
-        bookcaseCards
+        <div>
+        <h2 class="ml-5">Bookcases </h2>
+        <ul class="list-group ml-5">
+          {bookcaseCards}
+          </ul>
+        </div>
       )
   } else return <p>Loading...</p>
   }
