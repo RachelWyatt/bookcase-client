7872bae01039d86f11aef87c85865a5bec8a0c84
Rachel Wyatt <rw126@evansville.edu>
2019-09-05 08:06:03 -0400

    connect BookForm to store and handle input change in Redux

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,58 +1,63 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { addBook } from '../actions/bookForm.js'
+import { addBook, updateBookForm } from '../actions/bookForm.js'
 //
-const BookForm = () => (
+const BookForm = ({formData, history, updateBookForm}) => {
 //    state = {
 //        title: "",
 //        author: "",
 //        pageCount: "",
 //        spineColor: "",
 //    }
-//    handleChange = event => {
-//        this.setState({
-//            [event.target.name]: event.target.value
-//          })
-//    }
+    const handleChange = event => {
+        event.preventDefault()
+        const {name, value} = event.target
+       updateBookForm(name, value)
+    }
 //    handleSubmit = event => {
 //        event.preventDefault()
 //        this.props.addBook(this.state, this.props.bookcaseId, this.props.history)
 //    }
 //    
-//    
+    return (
         <div class="ml-5">
         <h2>Add a Book</h2>
-        < form onSubmit={this.handleSubmit}>
+        < form >
             <div class="form-group row">
                 <label for="bookFormTitleInput" class="col-sm-2 col-form-label">Title</label>
                 <div class="col-sm-6">
-                    <input placeholder="Title" class="form-control" name="title" value={this.props.title} type="text" onChange={this.handleChange}/> 
+                    <input placeholder="Title" class="form-control" name="title" value={formData.title} type="text" onChange={handleChange}/> 
                 </div>
             </div>
             <div class="form-group row" >
                 <label for="bookFormAuthorInput" class="col-sm-2 col-form-label">Author</label>
                 <div class="col-sm-6">
-                    <input placeholder="Author"  class="form-control" name="author" value={this.props.author} type="text" onChange={this.handleChange}/>
+                    <input placeholder="Author"  class="form-control" name="author" value={formData.author} type="text" onChange={handleChange}/>
                 </div>
             </div>
             <div class="form-group row" >
                 <label for="bookFormPageCountInput" class="col-sm-2 col-form-label">Page Count</label>
                 <div class="col-sm-6">
-                    <input placeholder="Page count"  class="form-control"  name="pageCount" value={this.props.pageCount} type="integer" onChange={this.handleChange}/>
+                    <input placeholder="Page count"  class="form-control"  name="pageCount" value={formData.pageCount} type="integer" onChange={handleChange}/>
                 </div>
             </div>
             <div class="form-group row" >
                 <label for="bookFormSpineColorInput" class="col-sm-2 col-form-label">Spine Color</label>
                 <div class="col-sm-6">
-                    <input placeholder="Spine color"  class="form-control"  name="spineColor" value={this.props.spineColor} type="text" onChange={this.handleChange}/>
+                    <input placeholder="Spine color"  class="form-control"  name="spineColor" value={formData.spineColor} type="text" onChange={handleChange}/>
                 </div>
             </div>
             <input type="submit" value="Submit" class="btn btn-outline-dark"/>
         </form>
         </div>
-        
     )
+        
+}
     
-      
+const mapStatetoProps = state => {
+    return {
+        formData: state.bookForm
+    }
+}
 
-export default connect(null, {addBook})(BookForm)
+export default connect(mapStatetoProps, {updateBookForm, addBook})(BookForm)
