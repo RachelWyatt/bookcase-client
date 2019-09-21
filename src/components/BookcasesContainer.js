72b15814ee72bebb63f6ea7956c288a4b6204eae
Rachel Wyatt <rw126@evansville.edu>
2019-09-21 12:18:03 -0400

    add key to each bookcase

--- src/components/BookcasesContainer.js
+++ src/components/BookcasesContainer.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 const Bookcases = props => {
   if (props.bookcases.length > 1) {
-      const bookcaseCards = props.bookcases.map(b => (<li className="list-group-item"><Link to={`/bookcases/${b.id}`} >{b.attributes.name}</Link></li>))
+      const bookcaseCards = props.bookcases.map(b => (<li className="list-group-item" key={b.id}><Link to={`/bookcases/${b.id}`} >{b.attributes.name}</Link></li>))
       return (
         <div>
         <h2 className="ml-5">Bookcases </h2>
